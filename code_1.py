40bbb75e27d65386903d80882a1b7bb4a2dc0289
kovaleski100 <mfkovaleski@inf.ufrgs.br>
2023-08-14 17:16:14 -0300

    dt implemented

--- code_1.py
+++ code_1.py
@@ -32,34 +32,76 @@ colluns = ["type",
            "20. There are people I can turn to"
 ]
 
+drops = ["uid", " gpa all",  " cs 65"]
+
+# colluns = ["type",
+#            "1. I feel in tune with the people around me",
+#            "2. I lack companionship",
+#            "3. There is no one I can turn to",
+#            "4. I do not feel alone",
+#            "5. I feel part of a group of friends",
+#            "6. I have a lot in common with the people around me",
+#            "7. I am no longer close to anyone",
+#            "8. My interests and ideas are not shared by those around me",
+#            "9. I am an outgoing person",
+#            "10. There are people I feel close to",
+#            "11. I feel left out",
+#            "12. My social relationships are superficial",
+#            "13. No one really knows me well",
+#            "14. I feel isolated from others",
+#            "15. I can find companionship when I want it",
+#            "16. There are people who really understand me",
+#            "17. I am unhappy being so withdrawn",
+#            "18. People are around me but not with me",
+#            "19. There are people I can talk to",
+#            "20. There are people I can turn to"
+# ]
+
 replys = {
-    "Often": 1,
-    "Rarely": 2,
-    "Sometimes": 3,
-    "Never": 4
+    "Often": "1",
+    "Rarely": "2",
+    "Sometimes": "3",
+    "Never": "4"
 }
 
 state = {
-    "pre": 1,
-    "post": 2
+    "pre": "1",
+    "post": "2"
 }
 
+def decisionTree(train, test):
 
-def models(train, test):
-    X_train, X_test, y_train, y_test = train_test_split(train, test, test_size=0.2, random_state=42)
-    pca = PCA(n_components=2)  # Defina o número de componentes principais desejados
-    X_train_pca = pca.fit_transform(X_train)
-    X_test_pca = pca.transform(X_test)
+    X_train,  y_train, = train_test_split(train, test_size=0.2, random_state=42)
+    #X_train, X_test, y_train, y_test = train_test_split(train, test, test_size=0.2, random_state=42)
+    #pca = PCA(n_components=2)  # Defina o número de componentes principais desejados
+    #X_train_pca = pca.fit_transform(X_train)
+    #X_test_pca = pca.transform(X_test)
     DT = DecisionTreeClassifier()
-    DT.fit(X_train_pca, y_train)
+    dataframeGPA = X_train[" gpa 13s"]
+    del X_train[" gpa 13s"]
+    #print(y_train)
+    DT.fit(X_train, dataframeGPA)
 
+    dataframeGPA = y_train[" gpa 13s"]
+    del y_train[" gpa 13s"]
+    
+    print(y_train)
+    print(dataframeGPA)
     # Fazer previsões usando os dados de teste reduzidos pelo PCA
-    y_pred = DT.predict(X_test_pca)
+    y_pred = DT.predict(y_train)
 
     # Calcular a acurácia
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Acurácia após PCA:", accuracy)
+    accuracy = accuracy_score(dataframeGPA, y_pred)
+    print("Acurácia:", accuracy)
 
+def convertGrades(dataframe, field):
+
+    dataframe[field] = dataframe[field].astype(str)
+    dataframe.loc[dataframe[field] == '4', field] = '4'
+    dataframe.loc[(dataframe[field] >= '3') & (dataframe[field] < '4'), field] = '3'
+    dataframe.loc[(dataframe[field] >= '2') & (dataframe[field] < '3'), field] = '2'
+    dataframe.loc[dataframe[field] < '2', field] = '0'
+    return dataframe
 
 def changeValueType(dataframe):
     for key in state:
@@ -75,11 +117,14 @@ def preProcess(dataframe):
     dataframe = changeValueType(dataframe)
     for i in range(1, len(colluns)):
         dataframe = changeValueQ1(dataframe, colluns[i])
+    dataframe = convertGrades(dataframe, " gpa 13s")
     types = dataframe.groupby(colluns[0])
     x = types.get_group(state["pre"])
     y = types.get_group(state["post"])
+
     #dataframe.loc[dataframe["1. I feel in tune with the people around me"]]
     return x, y
+
 def main():
     dirlist = ["education", "survey"]
     filesList = ["grades.csv", "LonelinessScale.csv"]
@@ -95,10 +140,13 @@ def main():
 
     tabelaco = pd.merge(dataframeList[0], dataframeList[1], on="uid", how="inner")
     dataframe = pd.DataFrame(tabelaco)
-    train, test = preProcess(dataframe)
-
+    for i in drops:
+        dataframe = dataframe.drop([i], axis=1)
     
-    models(train, test)
+
+    train, test = preProcess(dataframe)
+    
+    decisionTree(train, test)
 
 if __name__ == "__main__":
     main()
